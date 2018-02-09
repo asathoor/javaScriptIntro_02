5d725bd59807cc31a5205ad5840b2ba972f999ba
asathoor <per@multimusen.dk>
2018-02-09 10:29:18 +0100

    first

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+/**
+ * JavaScript introduktion: 02
+ * klasse: D2 
+ * dato: 2018.02.08
+ * lektioner: 6
+ **/
+
+// hvordan man skriver en liste
+
+// hvordan man skriver et element ud fra listen
+
+// hvordan man kan lave et loop der printer alle elementer ud fra listen
+
+// hvordan man afvikler en serie af kommandoer med en funktion
+
