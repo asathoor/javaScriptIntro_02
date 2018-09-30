b5c4b78fc8812e83793ace98c0e64a2f9e2761c7
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-09-30 20:37:39 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/events.js
+++ js/events.js
@@ -9,49 +9,34 @@
  * LISTER OG LOOPS
  **/
 
-// repetition: en variabel
-var foto = "foto.jpg";
-
 // hvordan man skriver en liste (filer til et galleri)
 var galleri = [
 	"foto.jpg",
 	"foto02.jpg"
-]; // NB: skarpe parenteser!
-
-// hvordan man skriver et element ud fra listen
-
-// hvordan man kan lave et loop der printer alle elementer ud fra listen
-for (i=0; i<galleri.length; i++) {
-	console.log( galleri[i] );
-}
+];
 
 // hvordan man afvikler en serie af kommandoer med en funktion
 function visBillede( billede ) {
-	document.write(
-		'<article>'
+    
+	document.write('<article>'
 		+ '<img src="images/'
 		+ billede
 		+ '" alt="billede" class="mini">'
-		+ '<article>'
-	);
+		+ '</article>');
+    console.log( "skriver fra billede" );
+    
 }
 
-visBillede( foto );
 
-/** 
- * EVENTS
- * @url: https://www.w3schools.com/jsref/dom_obj_event.asp
- **/
+// et loop
+for (var i=0;i<galleri.length;i++){
+    
+    visBillede( galleri[i] );
+}
 
-// husk på CSS :hover 
 
-// onclick (et eksempel på en event, mange andre er mulige)
 
-// hvordan JavaScript kan vælge i DOM (dokument-objekt-modellen)
-
-// hvordan man vælger et element i DOM 
-// og tilføjer en eventlistener og
-// hvordan man får en eventlistener til at fyre en funktion af
+// klik klik
 document.getElementById("etKlik").onclick = function() {myFunction()};
 
 // her er den funktion, der startes:
@@ -60,18 +45,3 @@ function myFunction() {
 		.getElementById("etKlik")
 		.innerHTML = "Du klikker på mig nu!";
 }
-
-/*
-KREATIV UDFORDRING: ANIMATION VIA CSS KLASSER TILFØJET MED JAVASCRIPT
-Brug en eventlistener og onclick - eller en anden event.
-Fremstil en animation for en CSS klasse.
-
-tip: element.addClass ...
-*/
-
-// hvordan man IKKE skal gøre
-// <button onclick="myFunction()">Click me</button>
-// jammen, det virker jo ...
-// htmf ... det er ikke elegant kode!
-
-
